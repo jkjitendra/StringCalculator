2acd8506bad68d11da2f11341356c9e6f3bc05e6
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-09-05 21:52:27 +0530

    - Added support for single number input - Added Test for single number

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,8 +2,10 @@ class StringCalculator {
   add(numbers) {
     if (numbers === "") {
       return 0;
+    } else if (!numbers.includes(",")) {
+      return parseInt(numbers, 10);
     }
   }
 }
-  
+
 module.exports = StringCalculator;
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -1,10 +1,15 @@
 const StringCalculator = require('./stringCalculator');
 
 describe('StringCalculator', () => {
-  
+
   test('should return 0 for an empty string', () => {
     const calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
+  });
+
+  test('should return the number itself for a single number', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("12")).toBe(12);
   });
 
 });
